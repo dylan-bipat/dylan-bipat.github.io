a84022f21581bbd158baa36ae044ee486622e5c2
Jaguirremori <J.aguirremori@gmail.com>
2020-04-02 19:22:59 -0400

    Update main.js

--- asst_2/main.js
+++ asst_2/main.js
@@ -17,14 +17,3 @@ images.forEach(image => {
 })
 
 /* Wiring up the Darken/Lighten button */
-btn.onclick = ()=>{
-    if (btn.getAttribute('class') == 'dark') {
-        btn.setAttribute('class', 'light');
-        btn.innerHTML = 'Lighten';
-        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    } else if (btn.getAttribute('class') == 'light') {
-        btn.setAttribute('class', 'dark');
-        btn.innerHTML = 'Darken';
-        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-    }
-}
